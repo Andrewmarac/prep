db8314217da5c3e5d1338a5e934bebe4a0deb506
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2020-11-30 20:53:21 +0000

    fatto

---
+++ EsempioMongo/client/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mflix-visualizer';
+  results : Object[];
+  obs : Observable<object>;
+  constructor(private http : HttpClient,private sanitizer: DomSanitizer){}
+
+  load10Movies()
+  {
+    this.obs = this.http.get("https://3000-bfc15c39-58be-49aa-a831-0dc9142b0882.ws-eu01.gitpod.io/movies");
+    this.obs.subscribe(this.getData);
+  }
+  load10Movieshorror()
+  {
+    this.obs = this.http.get("https://3000-bfc15c39-58be-49aa-a831-0dc9142b0882.ws-eu01.gitpod.io/moviehorror");
+    this.obs.subscribe(this.getData);
+  }
+  photoURL(urltoSanitize) {
+    console.log(urltoSanitize);
+    return this.sanitizer.bypassSecurityTrustUrl(urltoSanitize);
+}
+  getData = (data) => {
+    this.results = data;
+  }
+}
